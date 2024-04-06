a5dc0b7894a6f4f2da5bc1171f8ffb04e73dbd4f
Amey Bagwe <ameybagwe09@gmail.com>
2024-04-06 18:42:04 +0530

    Notifications and Firebase added

--- AppProgress.jsx
+++ AppProgress.jsx
@@ -1,14 +1,70 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, Image, SafeAreaView} from 'react-native';
+import {View, Text, Image, SafeAreaView, Alert} from 'react-native';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import {AnimatedCircularProgress} from 'react-native-circular-progress';
 import {Circle} from 'react-native-svg';
 import GradientText from './components/GradientText';
+import messaging from '@react-native-firebase/messaging';
+import PushNotification from 'react-native-push-notification';
+
+const channelId = 'default-channel-id';
 
 export default function AppProgress() {
   const [minutes, setMinutes] = useState(30);
   const [seconds, setSeconds] = useState(0);
   const [remainingTime, setRemainingTime] = useState(1800);
+  const [message, setMessage] = useState('');
+
+  async function requestUserPermission() {
+    try {
+      await messaging().requestPermission();
+      console.log('Notification permission granted');
+    } catch (error) {
+      console.log('Notification permission denied');
+    }
+  }
+
+  async function getToken() {
+    try {
+      const token = await messaging().getToken();
+      console.log('Token:', token);
+      return token;
+    } catch (error) {
+      console.log('Error getting token:', error);
+      return null;
+    }
+  }
+
+  useEffect(() => {
+    requestUserPermission();
+    getToken();
+    PushNotification.channelExists(channelId, exists => {
+      if (!exists) {
+        PushNotification.createChannel({
+          channelId: channelId,
+          channelName: 'Default Channel',
+          channelDescription: 'A default channel for notifications',
+          soundName: 'default',
+          importance: 4,
+          vibrate: true,
+        });
+      }
+    });
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      Alert.alert(
+        remoteMessage.notification.title,
+        remoteMessage.notification.body,
+      );
+      PushNotification.localNotification({
+        channelId: channelId,
+        title: remoteMessage.notification.title,
+        message: remoteMessage.notification.body,
+      });
+      setMessage(remoteMessage);
+    });
+
+    return unsubscribe;
+  }, []);
 
   useEffect(() => {
     const timer = setInterval(() => {
--- components/Tab.jsx
+++ components/Tab.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import {Tab} from '@rneui/themed';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import {StyleSheet} from 'react-native';
